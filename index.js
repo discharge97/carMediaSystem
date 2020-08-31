38c111f195e9ede05afd4d6f6ebdd30ee6b170db
Aleksandar Stojadinovic <aleksandar.stojadinovic.3641@metropolitan.ac.rs>
2020-08-31 22:04:11 +0200

    Added YT API, youtube post for server on '/youtube'

--- index.js
+++ index.js
@@ -2,37 +2,56 @@ const express = require('express');
 const Gpio = require('onoff').Gpio;
 const app = new express();
 const server = require('http').createServer(app);
+const cors = require('cors');
 const io = require('socket.io')(server);
+const bodyParser = require("body-parser");
 const rotaryEncoder = require('./encoder');
 
-const myEncoder = rotaryEncoder(5, 6, 7, 0); // Using BCM 5, BCM 6 & BCM 7 on the PI; two for the rotation and one for the push button
+//const myEncoder = rotaryEncoder(5, 6, 7, 0); // Using BCM 5, BCM 6 & BCM 7 on the PI; two for the rotation and one for the push button
 
-myEncoder.on('rotation', direction => {
-    if (direction > 0) {
-        // console.log('Encoder rotated right; clockwise');
-        selectNext();
+// myEncoder.on('rotation', direction => {
+//     if (direction > 0) {
+//         // console.log('Encoder rotated right; clockwise');
+//         selectNext();
+//     } else {
+//         // console.log('Encoder rotated left; counter clockwise');
+//         selectPrevious();
+//     }
+// });
+
+// myEncoder.on('click', pressState => {
+//     switch (pressState) {
+//         case 0:
+//             chooseOption();
+//             break;
+//         // case 1:
+//         //     console.log('Encoder button released');
+//         //     break;
+//     }
+// });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/youtube', (req, res) => {
+    if (playVideo(req.body.urlID)) {
+        playVideo(req.body.urlID);
+        res.sendStatus(200);
     } else {
-        // console.log('Encoder rotated left; counter clockwise');
-        selectPrevious();
-    }
-});
-
-myEncoder.on('click', pressState => {
-    switch (pressState) {
-        case 0:
-            chooseOption();
-            break;
-        // case 1:
-        //     console.log('Encoder button released');
-        //     break;
+        res.sendStatus(400);
     }
 });
 
 app.use(express.static(`${process.env.PWD}/public`));
 server.listen(8080);
 
+io.on("connection", (sock) => {
+    console.log(sock.id);
+})
+
 function selectNext() {
     io.emit("next", { next: true });
+    console.log("next");
 }
 
 function selectPrevious() {
@@ -42,3 +61,11 @@ function selectPrevious() {
 function chooseOption() {
     io.emit("choose", { choose: true });
 }
+
+function playVideo(urlID) {
+    io.emit("youtube", urlID);
+}
+
+// setInterval(() => {
+//     selectNext();
+// }, 1000)
--- public/brainz.js
+++ public/brainz.js
@@ -1,17 +1,19 @@
-const { io } = require("./socket.io");
-
-document.onkeypress = handleMenu;
-let socket = io('http://localhost');
-let index = 0;
-const menuItems = {};
+const socket = io();
+var index = 0;
+var menuItems = {};
 initMenu();
 
+socket.on('connect', () => {
+    console.log("hit");
+});
 
 socket.on("next", () => {
+    console.log("next");
     handleMenu(1);
 });
 
 socket.on("prev", () => {
+    console.log("prev");
     handleMenu(-1);
 });
 
@@ -19,9 +21,75 @@ socket.on("choose", () => {
     chooseCurrentMenuItem();
 });
 
+socket.on("youtube", data => {
+    playVideo(data);
+});
+
+//  ========== YOUTUBE LOGIC ==========
+
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {
+        stopYTVideo();
+    }
+}
+
+function stopYTVideo() {
+    player.stopVideo();
+}
+
+function playVideo(urlOrID = "") {
+    if (urlID.startsWith("http")) {
+        urlOrID = getYouTubeID(urlOrID);
+    }
+
+    player.loadVideoById(urlOrID);
+    player.playVideo();
+}
+
+function getYouTubeID(url) {
+    var ID = '';
+    url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+    if (url[2] !== undefined) {
+        ID = url[2].split(/[^0-9a-z_\-]/i);
+        ID = ID[0];
+    }
+    else {
+        ID = url;
+    }
+    return ID;
+}
+//  ========== YOUTUBE LOGIC ==========
+
+
 function handleMenu(val) {
+    if (!menuItems || !menuItems.items || !menuItems.panels) initMenu();
     index += val;
-    if (menuItems.items == undefined || menuItems.panels == undefined) initMenu();
 
     if (index >= menuItems.items.length) {
         index = 0;
@@ -55,6 +123,8 @@ function chooseCurrentMenuItem(forceSelect = false) {
     switch (index) {
         case 2:
             // TODO: stop YT player 
+            stopYTVideo();
+            stopVi
             break;
 
         default:
